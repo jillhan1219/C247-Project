a8a28386fd032ad77003e7112cf3a3efc4a0e69a
Jenny Ho <jennyho0221@gmail.com>
2026-03-11 17:42:47 -0700

    experiments with cnn + rnn code and nb

--- jenny_cnn_rnn_exp/code/modules.py
+++ jenny_cnn_rnn_exp/code/modules.py
@@ -294,3 +294,28 @@ class GRUOutputOnly(nn.Module):
     def forward(self, x):
         y, _ = self.gru(x)
         return y
+
+
+class LSTMOutputOnly(nn.Module):
+    def __init__(
+        self,
+        input_size,
+        hidden_size,
+        num_layers=2,
+        bidirectional=True,
+        dropout=0.1,
+    ):
+        super().__init__()
+
+        self.lstm = nn.LSTM(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            bidirectional=bidirectional,
+            dropout=dropout if num_layers > 1 else 0.0,
+        )
+
+    def forward(self, x):
+        # x: (T, N, C)
+        outputs, _ = self.lstm(x)
+        return outputs
